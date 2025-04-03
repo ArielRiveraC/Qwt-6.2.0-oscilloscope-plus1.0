efffc60eb2bd475037b3b979ecb5e52c913e1fd2
Ariel <ariverac@uni.pe>
2025-04-02 20:40:33 -0500

    27-03-2025

---
+++ Qwt-6.2.0/qwt-6.2.0/examples/oscilloscope/MainWindow.h
@@ -0,0 +1,65 @@
+/*****************************************************************************
+ * Qwt Examples - Copyright (C) 2002 Uwe Rathmann
+ * This file may be used under the terms of the 3-clause BSD License
+ *****************************************************************************/
+
+#pragma once
+
+#include <QWidget>
+#include <QPushButton>
+#include <QCheckBox>
+#include <QProcess>
+#include <QDateTime>
+#include <QListWidget>
+#include <QPixmap>
+
+#include "SamplingThread.h"
+
+class Plot;
+class Knob;
+class WheelBox;
+class QLabel;
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+
+  public:
+    MainWindow( QWidget* = NULL );
+
+    void start();
+
+    double amplitude() const;
+    double frequency() const;
+    double signalInterval() const;
+
+    void getSamplingThreadptr(SamplingThread*);
+
+    void handle_Stopbutton();
+    void handle_Capturebutton();
+    void handle_ChangeColorbutton();
+    void handle_legendItem_checkBox();
+    void handle_ItemClicked();
+    void update_Curvetitle();
+
+  Q_SIGNALS:
+    void amplitudeChanged( double );
+    void frequencyChanged( double );
+    void signalIntervalChanged( double );
+
+  private:
+    bool m_ispause = false;
+    Knob* m_frequencyKnob;
+    Knob* m_amplitudeKnob;
+    WheelBox* m_timerWheel;
+    WheelBox* m_intervalWheel;
+    QPushButton* m_Stopbutton;
+    QPushButton* m_Capturebutton;
+    QPushButton* m_ChangeColorbutton;
+    QCheckBox* m_legendItem_checkBox;
+    SamplingThread* m_samplingThread;
+    QListWidget* m_captureListWidget;
+    QVector<QString>* m_vectorstring;
+
+    Plot* m_plot;
+};
---
+++ Qwt-6.2.0/qwt-6.2.0/examples/oscilloscope/Plot.h
@@ -0,0 +1,81 @@
+/*****************************************************************************
+ * Qwt Examples - Copyright (C) 2002 Uwe Rathmann
+ * This file may be used under the terms of the 3-clause BSD License
+ *****************************************************************************/
+
+#pragma once
+
+#include <QwtPlot>
+#include <QVector>
+#include <QBuffer>
+#include <QDebug>
+#include <QwtInterval>
+#include <QElapsedTimer>
+
+#include "Settings.h"
+#include "MainWindow.h"
+
+class WheelBox;
+class QwtPlotCurve;
+class QwtPlotMarker;
+class QwtPlotDirectPainter;
+class QwtPlotLegendItem;
+
+class Plot : public QwtPlot
+{
+    Q_OBJECT
+
+  public:
+    Plot( QWidget* = NULL );
+    virtual ~Plot();
+
+    void start();
+    virtual void replot() QWT_OVERRIDE;
+
+    virtual bool eventFilter( QObject*, QEvent* ) QWT_OVERRIDE;
+
+    void setColorCurve(QColor color);
+    void setCurveTitle(QString);
+    bool setStartTimer();
+
+
+  public Q_SLOTS:
+    void setIntervalLength(double);
+    void updateItemLegend(bool);
+    void getplotScreenShot(QString, int, int);
+    QBuffer* getimagefromqbuffer(int);
+    WheelBox* getLevelWheel();
+
+  protected:
+    virtual void showEvent( QShowEvent* ) QWT_OVERRIDE;
+    virtual void resizeEvent( QResizeEvent* ) QWT_OVERRIDE;
+    virtual void timerEvent( QTimerEvent* ) QWT_OVERRIDE;
+
+  Q_SIGNALS:
+    void edited();
+
+  private Q_SLOTS:
+    void scrollLeftAxis( double );
+
+  private:
+    void updateCurve();
+    void incrementInterval();
+
+    QwtPlotCurve* m_curve;
+    QwtPlotMarker* m_origin;
+    WheelBox* m_levelWheel;
+    QwtPlotLegendItem* m_legendItem;
+    bool m_isDirty;
+
+    QBuffer* m_qbuffer;
+    QVector<QBuffer*>* m_qvectorqbuffer;
+
+    int m_paintedPoints;
+
+    QwtPlotDirectPainter* m_directPainter;
+
+    QwtInterval m_interval;
+    int m_timerId;
+
+    QElapsedTimer m_elapsedTimer;
+};
