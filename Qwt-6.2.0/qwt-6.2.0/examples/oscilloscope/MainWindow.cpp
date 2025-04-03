892c6fea4a214edd891341b15142d2153ba4d883
Ariel <ariverac@uni.pe>
2025-04-02 22:22:28 -0500

    02-04-2025

--- Qwt-6.2.0/qwt-6.2.0/examples/oscilloscope/MainWindow.cpp
+++ Qwt-6.2.0/qwt-6.2.0/examples/oscilloscope/MainWindow.cpp
@@ -77,13 +77,13 @@ MainWindow::MainWindow( QWidget* parent )
              &MainWindow::handle_legendItem_checkBox);
 
     connect( m_amplitudeKnob, SIGNAL(valueChanged(double)),
-        SIGNAL(amplitudeChanged(double)) );
+             SIGNAL(amplitudeChanged(double)) );
     connect( m_frequencyKnob, SIGNAL(valueChanged(double)),
-        SIGNAL(frequencyChanged(double)) );
+             SIGNAL(frequencyChanged(double)) );
     connect( m_timerWheel, SIGNAL(valueChanged(double)),
-        SIGNAL(signalIntervalChanged(double)) );
+             SIGNAL(signalIntervalChanged(double)) );
     connect( m_intervalWheel, SIGNAL(valueChanged(double)),
-        m_plot, SLOT(setIntervalLength(double)) );
+             m_plot, SLOT(setIntervalLength(double)) );
 }
 
 void MainWindow::start()
